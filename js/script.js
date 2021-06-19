216f35fbef5eb5e9e2048678a4789a1e29f2c58b
Georgy132 <86150520+Georgy132@users.noreply.github.com>
2021-06-19 15:22:55 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const modal = document.querySelector('.modal'),
+      modalbtn = document.querySelector('.header__btn'),
+      modalClose = document.querySelector('.modal__close');
+      overlayDark = document.querySelector('.overlay__dark');
+
+modalbtn.addEventListener('click', ()=>{
+    modal.style = 'display: block';
+    overlayDark.style = 'display: block';
+});
+
+modalClose.addEventListener('click', ()=>{
+    modal.style = 'display: none';
+    overlayDark.style = 'display: none';
+})
+
+
+$('form').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+    }).done(function() {
+        $(this).find("input").val("");
+        $('#sender').fadeOut();
+        $('.overlay__dark, #thanks').fadeIn('slow');
+        $('form').trigger('reset');
+    });
+    return false;
+});
