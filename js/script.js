3f495b48fca7167b501bde93f5c9de8295957912
georgyurfu <86150520+georgyurfu@users.noreply.github.com>
2021-06-19 21:40:28 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
 const modal = document.querySelector('.modal'),
       modalbtn = document.querySelector('.header__btn'),
-      modalClose = document.querySelector('.modal__close');
+      modalClose = document.querySelector('.modal__close'),
       overlayDark = document.querySelector('.overlay__dark');
+
+
+
+$('form').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+    }).done(function() {
+        $(this).find("input").val("");
+        $('#sender').fadeOut();
+        $('.overlay__dark, #thanks').fadeIn();
+        $('.overlay__dark, #thanks').fadeOut( 4000 );
+        $('form').trigger('reset');
+    });
+    return false;
+});
 
 modalbtn.addEventListener('click', ()=>{
     modal.style = 'display: block';
@@ -13,19 +31,3 @@ modalClose.addEventListener('click', ()=>{
     modal.style = 'display: none';
     overlayDark.style = 'display: none';
 })
- 
-
-$('form').submit(function(e) {
-    e.preventDefault();
-    $.ajax({
-        type: "POST",
-        url: "mailer/smart.php",
-        data: $(this).serialize()
-    }).done(function() {
-        $(this).find("input").val("");
-        $('#sender').fadeOut();
-        $('.overlay__dark, #thanks').fadeIn('slow');
-        $('form').trigger('reset');
-    });
-    return false;
-});
